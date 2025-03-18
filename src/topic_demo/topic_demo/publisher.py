9f6d5c2d468059a76ef8d15743b3e9b632541e91
Jesse <2604243189zjx@gmail.com>
2025-03-18 19:33:49 +0800

    修改为温度读取模拟器

--- src/topic_demo/topic_demo/publisher.py
+++ src/topic_demo/topic_demo/publisher.py
@@ -1,28 +1,30 @@
 # publisher.py
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32
+import random
 
 class MinimalPublisher(Node):
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'test_topic', 10)
+        super().__init__('temperature_publisher')
+        self.publisher_ = self.create_publisher(Float32, 'temperature', 10)
         timer_period = 1.0  # 1秒周期
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.temperature = 20.0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = f'Hello World: {self.i}'
+        # 模拟温度变化
+        self.temperature += random.uniform(-1.0, 1.0)
+        msg = Float32()
+        msg.data = self.temperature
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: "{msg.data}"')
-        self.i += 1
+        self.get_logger().info(f'Publishing: {msg.data}')
 
 def main(args=None):
     rclpy.init(args=args)
-    minimal_publisher = MinimalPublisher()
-    rclpy.spin(minimal_publisher)
-    minimal_publisher.destroy_node()
+    temperature_publisher = MinimalPublisher()
+    rclpy.spin(temperature_publisher)
+    temperature_publisher.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- src/topic_demo/topic_demo/subscriber.py
+++ src/topic_demo/topic_demo/subscriber.py
@@ -1,26 +1,26 @@
 # subscriber.py
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import Float32
 
 class MinimalSubscriber(Node):
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('temperature_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            'test_topic',
+            Float32,
+            'temperature',
             self.listener_callback,
             10)
         self.subscription  # 防止未使用变量警告
 
     def listener_callback(self, msg):
-        self.get_logger().info(f'I heard: "{msg.data}"')
+        self.get_logger().info(f'温度: "{msg.data}"')
 
 def main(args=None):
     rclpy.init(args=args)
-    minimal_subscriber = MinimalSubscriber()
-    rclpy.spin(minimal_subscriber)
-    minimal_subscriber.destroy_node()
+    temperature_subscriber  = MinimalSubscriber()
+    rclpy.spin(temperature_subscriber)
+    temperature_subscriber.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
